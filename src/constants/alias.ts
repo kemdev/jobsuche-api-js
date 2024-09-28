5aa3684c305e962255855295ae69915107e4b4a4
kemdev <aa171@students.kiron.ngo>
2024-09-28 15:35:46 +0200

    Update yml

--- src/constants/alias.ts
+++ src/constants/alias.ts
@@ -1,19 +1,19 @@
-import { IFetchJobsProps } from '../types/jobsSearchTypes';
+import { IFetchJobsProps } from "../types/jobsSearchTypes";
 
 const aliasMapping: Record<keyof IFetchJobsProps, string> = {
-  what: 'was',
-  where: 'wo',
-  professionalField: 'berufsfeld',
-  page: 'page',
-  employer: 'arbeitgeber',
-  isTemporary: 'zeitarbeit',
-  size: 'size',
-  publishedSince: 'veroeffentlichtseit',
-  isPrivate: 'pav',
-  offerType: 'angebotsart',
-  contractDuration: 'befristung',
-  radius: 'umkreis',
-  workTime: 'arbeitszeit',
+  what: "was",
+  where: "wo",
+  professionalField: "berufsfeld",
+  page: "page",
+  employer: "arbeitgeber",
+  isTemporary: "zeitarbeit",
+  size: "size",
+  publishedSince: "veroeffentlichtseit",
+  isPrivate: "pav",
+  offerType: "angebotsart",
+  contractDuration: "befristung",
+  radius: "umkreis",
+  workTime: "arbeitszeit",
 };
 
 export { aliasMapping };
